5f337de223fec13e4ef65039852db2f8881f91ec
Sebastian Blaszczyk <s.blaszczyk.633@studms.ug.edu.pl>
2025-01-04 19:58:30 +0100

    Added tranings endpoints

--- app/routes/training_routes.py
+++ app/routes/training_routes.py
@@ -9,7 +9,7 @@ def get_trainings(email):
         return jsonify({"message": "Account not found"}), 404
     return jsonify({"trainings": account.trainings}), 200
 
-@app.route("/archer/<email>/training", methods=['PUT'])
+@app.route("/archer/<email>/trainings/add", methods=['POST'])
 def add_training(email):
     data = request.get_json()
     print(f"Add training request: {data}")
@@ -18,3 +18,26 @@ def add_training(email):
         return jsonify({"message": "Account not found"}), 404
     account.add_training(data["quantity_of_shots"], data["distance"])
     return jsonify({"message": "Training added"}), 200
+
+@app.route("/archer/<email>/trainings/delete", methods=['DELETE'])
+def delete_training(email):
+    data = request.get_json()
+    print(f"Delete training request: {data}")
+    account = ArcherRegistry.find_account_by_email(email)
+    if account is None:
+        return jsonify({"message": "Account not found"}), 404
+    account.delete_training(data["date"])
+    return jsonify({"message": "Training deleted"}), 200
+
+@app.route("/archer/<email>/trainings/change", methods=['PUT'])
+def change_training(email):
+    data = request.get_json()
+    print(f"Change training request: {data}")
+    account = ArcherRegistry.find_account_by_email(email)
+    if account is None:
+        return jsonify({"message": "Account not found"}), 404
+    account.delete_training(data["date"])
+    account.add_training(data["quantity_of_shots"], data["distance"])
+    return jsonify({"message": "Training changed"}), 200
+
+
